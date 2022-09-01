baed435b10fa922daaf1e2d58942688007ffe3dc
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-01 18:12:04 +0200

    Commit new printLn

--- src/main/java/CommitTesting.java
+++ src/main/java/CommitTesting.java
@@ -1,6 +1,6 @@
 public class CommitTesting {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello Ilker Yildirim");
     }
 }
