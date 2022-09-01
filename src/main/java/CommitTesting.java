c2273168ae05ff9555d55f3a6432c30acdc53248
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-01 18:25:34 +0200

    Second branch

--- src/main/java/CommitTesting.java
+++ src/main/java/CommitTesting.java
@@ -2,5 +2,11 @@ public class CommitTesting {
     public static void main(String[] args) {
 
         System.out.println("Hello Ilker Yildirim");
+
+
+        int x = 5;
+        int y = 16;
+
+        System.out.println(x + y);
     }
 }
